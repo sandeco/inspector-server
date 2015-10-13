95adee13971675ae4ea9564f592398c71724ab3d
sandeco <sandecom@gmail.com>
2015-10-13 14:06:31 -0300

    Ajustando controlador de participacoes

--- src/com/inspector/controller/ParticipanteController.java
+++ src/com/inspector/controller/ParticipanteController.java
@@ -1,81 +1,19 @@
 package com.inspector.controller;
 
-import java.io.IOException;
-import java.util.List;
-
-import org.codehaus.jackson.JsonParseException;
-import org.codehaus.jackson.map.JsonMappingException;
-import org.codehaus.jackson.type.TypeReference;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.inspector.model.RespostaPersistencia;
-import com.inspector.modelCom.ParticipacaoCom;
-import com.inspector.persistence.dao.ParticipacaoDAO;
-import com.inspector.persistence.jpa.ParticipacaoDaoJPA;
-import com.inspector.util.Json;
+import com.inspector.model.Participante;
+import com.inspector.persistence.dao.ParticipanteDAO;
 
 @RequestMapping("/participante")
 @RestController
-public class ParticipanteController {
+public class ParticipanteController extends GenericController<Participante, Integer>{
 
-	private RespostaPersistencia resposta = new RespostaPersistencia();
-	private ParticipacaoDAO dao = new ParticipacaoDaoJPA();
-
-	@RequestMapping()
-	public RespostaPersistencia receberParticipacoes(@RequestParam String participacoes){
-
-		List<ParticipacaoCom> list = null;
-		resposta.setError(true);
-
-		try {
-			list = Json.mapper.readValue(participacoes, new TypeReference<List<ParticipacaoCom>>(){});
-			dao.salvarParticipacoes(list);
-			resposta.setError(false);
-			resposta.setMensagem("Participações salvas com sucesso no servidor!");
-			resposta.setSize(list.size());
-			
-		} catch (JsonParseException e) {
-			resposta.setMensagem(e.getMessage());
-			e.printStackTrace();
-		} catch (JsonMappingException e) {
-			resposta.setMensagem(e.getMessage());
-			e.printStackTrace();
-		} catch (IOException e) {
-			resposta.setMensagem(e.getMessage());
-			e.printStackTrace();
-		} catch (Exception e) {
-			resposta.setMensagem(e.getMessage());
-			e.printStackTrace();
-		}
-
-
-		return resposta;
+	@Autowired
+	public ParticipanteController(ParticipanteDAO dao) {
+		super(dao);
 	}
 
 }
-
-
-/*
-Participacao p1 = new Participacao();
-Participacao p2 = new Participacao();
-
-Ministracao m = new Ministracao();
-m.setLocal("sala xxx");
-Participante pa = new Participante();
-pa.setNome("Sanderson");
-pa.setCpf("954875398573");
-
-p1.setMinistracao(m);
-p1.setParticipante(pa);
-
-p2.setMinistracao(m);
-p2.setParticipante(pa);
-
-
-List<Participacao> list = new ArrayList<Participacao>();
-
-list.add(p1);
-list.add(p2);
- */
--- src/com/inspector/modelCom/ParticipacaoCom.java
+++ src/com/inspector/modelCom/ParticipacaoCom.java
@@ -23,5 +23,4 @@ public class ParticipacaoCom {
 		this.idParticipante = idParticipante;
 	}
     
-    
 }
